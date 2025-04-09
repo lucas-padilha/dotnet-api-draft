87a16dfe909ac99a0f706c50a1a87ce8f437db75
lucas-padilha <lukks.henrique@gmail.com>
2025-04-09 12:39:07 -0300

    adding data notations| cleaning configs

--- dotnet-api-draft/Models/Filme.cs
+++ dotnet-api-draft/Models/Filme.cs
@@ -1,9 +1,17 @@
-﻿namespace Draft.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Draft.Model;
 
 public class Filme
 {
 public Guid Id { get; set; }
+[Required(ErrorMessage = "O campo Titulo é obrigatório.")]
 public string? Titulo { get; set; }
+[Required(ErrorMessage = "O campo Genero é obrigatório.")]
+[MaxLength(50, ErrorMessage = "O campo Genero deve ter no máximo 50 caracteres.")]
 public string? Genero { get; set; }  
+[Required(ErrorMessage = "O campo Duracao é obrigatório.")]
+[Range(1, 300, ErrorMessage = "O campo Duracao deve ser um número entre 15 e 300.")]
+[Display(Name = "Duração em minutos")]
 public int Duracao { get; set; }
 }
