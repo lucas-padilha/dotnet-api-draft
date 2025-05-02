a599e600932e76ecd970f9c703d8ace9954ebefe
lucas-padilha <lukks.henrique@gmail.com>
2025-05-02 07:00:35 -0300

    feat: wrap the project in modules | apply references e packages

---
+++ draft-infra/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿
+using DraftDomain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DraftInfra.Data;
+
+public class FilmeContext :DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Filme> Filmes { get; set; } = null!;
+    
+}
